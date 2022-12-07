4612405b91f76ba353e4b1e974dd101f9c53eb96
DariaG16 <dariagolitsyna16@gmail.com>
2022-12-07 13:55:41 -0500

    fixed it, then broke it IMMEDIATELY after

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,3 +1,16 @@
 import "../styles/style.css";
 import { cookies } from "./cookies";
 console.log(cookies);
+
+let sorting = cookies.filter((cookies) => cookies.usefulIn.includes("arena"));
+sorting.forEach((cookies) => );
+
+sorting.insertAdjacentHTML(
+    `beforeend`,
+    `
+    <div class="output1">
+    <h2 id="cookieNam" class="output" >Cookie:</h2>
+    <p id="rarityOut" class="output">Rarity: ${}</p> 
+    <p id="skillTypeOut" class="output">Skill Type: ${}</p> 
+    <button id="remove">REMOVE COOKIE</button></div>`
+  ); //whatever is put in the doughtype box comes out under it
