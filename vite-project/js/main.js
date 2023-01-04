0508c1b2c4349d35494a3a72415c6660d36f2f82
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2023-01-03 19:42:39 -0500

    I FIX.

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -68,128 +68,26 @@ const rarities = [
   "Click here!",
 ];
 if (temp1 == 0) {
-  cookies.forEach((cookies) =>
-    DOMselectors.everything.insertAdjacentHTML(
-      "beforeend",
-      `<div class="Category cards" >
-      <h3>Type: ${cookies.type}</h3>
-      <h3>Rarity: ${cookies.rarity}</h3>
-      <h3>Useful In: ${cookies.usefulIn}</h3>
-      <img class="images" src="${cookies.img}" alt="${cookies.name}">
-      <h2>${cookies.name}</h2>
-      </div>`
-    )
-  );
+  makeCards(cookies);
 }
 DOMselectors.raritybtn.addEventListener("click", function () {
   temp1++;
   DOMselectors.raritybtn.innerHTML = rarities[temp1];
   if (temp1 == 1) {
-    removeCard();
-    common.forEach((cookies) =>
-      DOMselectors.everything.insertAdjacentHTML(
-        "beforeend",
-        `<div class="Category cards" >
-      <h3>Type: ${cookies.type}</h3>
-      <h3>Rarity: ${cookies.rarity}</h3>
-      <h3>Useful In: ${cookies.usefulIn}</h3>
-      <img class="images" src="${cookies.img}" alt="${cookies.name}">
-      <h2>${cookies.name}</h2>
-      </div>`
-      )
-    );
-  }
-  if (temp1 == 2) {
-    removeCard();
-    rare.forEach((cookies) =>
-      DOMselectors.everything.insertAdjacentHTML(
-        "beforeend",
-        `<div class="Category cards" >
-      <h3>Type: ${cookies.type}</h3>
-      <h3>Rarity: ${cookies.rarity}</h3>
-      <h3>Useful In: ${cookies.usefulIn}</h3>
-      <img class="images" src="${cookies.img}" alt="${cookies.name}">
-      <h2>${cookies.name}</h2>
-      </div>`
-      )
-    );
-  }
-  if (temp1 == 3) {
-    removeCard();
-    epic.forEach((cookies) =>
-      DOMselectors.everything.insertAdjacentHTML(
-        "beforeend",
-        `<div class="Category cards" >
-      <h3>Type: ${cookies.type}</h3>
-      <h3>Rarity: ${cookies.rarity}</h3>
-      <h3>Useful In: ${cookies.usefulIn}</h3>
-      <img class="images" src="${cookies.img}" alt="${cookies.name}">
-      <h2>${cookies.name}</h2>
-      </div>`
-      )
-    );
-  }
-  if (temp1 == 4) {
-    removeCard();
-    superEpic.forEach((cookies) =>
-      DOMselectors.everything.insertAdjacentHTML(
-        "beforeend",
-        `<div class="Category cards" >
-      <h3>Type: ${cookies.type}</h3>
-      <h3>Rarity: ${cookies.rarity}</h3>
-      <h3>Useful In: ${cookies.usefulIn}</h3>
-      <img class="images" src="${cookies.img}" alt="${cookies.name}">
-      <h2>${cookies.name}</h2>
-      </div>`
-      )
-    );
-  }
-  if (temp1 == 5) {
-    removeCard();
-    legendary.forEach((cookies) =>
-      DOMselectors.everything.insertAdjacentHTML(
-        "beforeend",
-        `<div class="Category cards" >
-      <h3>Type: ${cookies.type}</h3>
-      <h3>Rarity: ${cookies.rarity}</h3>
-      <h3>Useful In: ${cookies.usefulIn}</h3>
-      <img class="images" src="${cookies.img}" alt="${cookies.name}">
-      <h2>${cookies.name}</h2>
-      </div>`
-      )
-    );
-  }
-  if (temp1 == 6) {
-    removeCard();
-    ancient.forEach((cookies) =>
-      DOMselectors.everything.insertAdjacentHTML(
-        "beforeend",
-        `<div class="Category cards" >
-      <h3>Type: ${cookies.type}</h3>
-      <h3>Rarity: ${cookies.rarity}</h3>
-      <h3>Useful In: ${cookies.usefulIn}</h3>
-      <img class="images" src="${cookies.img}" alt="${cookies.name}">
-      <h2>${cookies.name}</h2>
-      </div>`
-      )
-    );
-  }
-  if (temp1 == 7) {
-    removeCard();
-    special.forEach((cookies) =>
-      DOMselectors.everything.insertAdjacentHTML(
-        "beforeend",
-        `<div class="Category cards" >
-      <h3>Type: ${cookies.type}</h3>
-      <h3>Rarity: ${cookies.rarity}</h3>
-      <h3>Useful In: ${cookies.usefulIn}</h3>
-      <img class="images" src="${cookies.img}" alt="${cookies.name}">
-      <h2>${cookies.name}</h2>
-      </div>`
-      )
-    );
-  }
-  if (temp1 > 7) {
+    makeCards(common);
+  } else if (temp1 == 2) {
+    makeCards(rare);
+  } else if (temp1 == 3) {
+    maceCards(epic);
+  } else if (temp1 == 4) {
+    maceCards(superEpic);
+  } else if (temp1 == 5) {
+    maceCards(legendary);
+  } else if (temp1 == 6) {
+    maceCards(ancient);
+  } else if (temp1 == 7) {
+    maceCards(special);
+  } else if (temp1 > 7) {
     removeCard();
     temp1 = 0;
   }
@@ -211,22 +109,22 @@ const usefulIn = [
 DOMselectors.usefulInBtn.addEventListener("click", function () {
   temp3++;
   DOMselectors.usefulInBtn.innerHTML = usefulIn[temp3];
-  if (temp2 == 1) {
-    makeCards2(usefulInArena);
-  } else if (temp2 == 2) {
-    makeCards2(usefulInWE);
-  } else if (temp2 == 3) {
-    makeCards2(usefulInOdyssey);
-  } else if (temp2 == 4) {
-    makeCards2(usefulInTOSC);
-  } else if (temp2 == 5) {
-    makeCards2(usefulInGuild);
-  } else if (temp2 == 6) {
-    makeCards2(usefulInDecor);
-  } else if (temp2 == 7) {
-    makeCards2(usefulInBC);
-  } else if (temp2 == 8) {
-    makeCards2(usefulInMM);
+  if (temp3 == 1) {
+    maceCards(usefulInArena);
+  } else if (temp3 == 2) {
+    maceCards(usefulInWE);
+  } else if (temp3 == 3) {
+    maceCards(usefulInOdyssey);
+  } else if (temp3 == 4) {
+    maceCards(usefulInTOSC);
+  } else if (temp3 == 5) {
+    maceCards(usefulInGuild);
+  } else if (temp3 == 6) {
+    maceCards(usefulInDecor);
+  } else if (temp3 == 7) {
+    maceCards(usefulInBC);
+  } else if (temp3 == 8) {
+    maceCards(usefulInMM);
   } else if (temp3 > 8) {
     removeCard();
     temp3 = 0;
@@ -283,21 +181,9 @@ document
   .querySelector("#lightDark")
   .addEventListener("click", function helpPlease() {
     if (document.body.classList.contains("warm")) {
-      const cards = document.querySelectorAll(".cards");
-      const cardsArray = Array.from(cards);
-      cardsArray.forEach((cards) => {
-        cards.style.cssText =
-          "  box-shadow: 10px 5px 5px var(--darker-blue); color: var(--details); background-color: var(--card); border: 4px solid var(--details);";
-      });
       document.body.classList.add("cool");
       document.body.classList.remove("warm");
     } else {
-      const cards = document.querySelectorAll(".cards");
-      const cardsArray = Array.from(cards);
-      cardsArray.forEach((cards) => {
-        cards.style.cssText =
-          "box-shadow: 10px 5px 5px var(--darker-brown); color: var(--dark-brown); background-color: var(--light-brown);border: 4px solid var(--dark-green);";
-      });
       document.body.classList.add("warm");
       document.body.classList.remove("cool");
     }
@@ -318,7 +204,7 @@ function makeCards(type) {
     )
   );
 }
-function makeCards2(usefulIn) {
+function maceCards(usefulIn) {
   removeCard();
   usefulIn.forEach((cookies) =>
     DOMselectors.everything.insertAdjacentHTML(
@@ -333,52 +219,3 @@ function makeCards2(usefulIn) {
     )
   );
 }
-
-/* cookies.forEach((cookies) =>
-  DOMselectors.everything.insertAdjacentHTML(
-    "beforeend",
-    `<div class="Category cards" >
-      <h3>Type: ${cookies.type}</h3>
-      <h3>Rarity: ${cookies.rarity}</h3>
-      <h3>Useful In: ${cookies.usefulIn}</h3>
-      <img class="images" src="${cookies.img}" alt="${cookies.name}">
-      <h2>${cookies.name}</h2>
-      </div>`
-  )
-); */
-
-/* const sorting = {
-  getAllProducts: null,
-  getCookies: function () {
-    cookies.filter((cookies) => cookies.type.includes(`charge`));
-    cookies.forEach((cookies) => {
-      console.log(cookies.name);
-      DOM.everything.insertAdjacentHTML = `<h1>${cookies.name}</h1>`;
-    });
-  },
-}; 
-sorting.getCookies(); */
-//create an array :D
-//create a funciton to inject menu item :D
-//iterate (foreach) over array and insert HTML
-/* const result = cookies.filter((cookie) => cookie.skill === "ranged"); //filters the cookies based on their skills
-console.log(result);
-
-cookies.forEach((cookies) => {
-  DOM.everything.insertAdjacentHTML(
-    afterend,
-    `<div class="Category child-1">
-     <h3>Type: Defense</h3>
-    <img src="/images/hollyberry.png" alt="Hollyberry Cookie" />
-    <h3>Rarity: Ancient</h3>
-    <h2>Hollyberry cookie</h2>
-    </div>`
-  );
-  console.log(cookies.name);
-  console.log(cookies.skill);
-  cookies.usefulIn.forEach((usefulIn) => {
-    console.log(usefulIn);
-  });
-}); */
-//button event listen to filter
-//OOP object
